2d5bcb91641cfddfe8c12d63a1ce08d12dcb8c9c
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-02 18:08:11 +0300

    Page setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+      <div className="bg-indigo-200 min-h-screen">
+          <h4>App</h4>
+      </div>
+  );
+};
+
+export default App;
