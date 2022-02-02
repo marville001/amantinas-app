b0c51b040d033231071da19e5ebb65a277bb1d7a
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-02 23:42:37 +0300

    Add to bar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,15 @@ import DashboardWrapper from "../../components/DashboardWrapper/DashboardWrapper
 const Home = () => {
   return (
     <DashboardWrapper>
-      <h4>Home</h4>
+      <div className="my-10">
+        <div className="grid space-y-5 lg:space-y-0 grid-cols-1 lg:grid-cols-4 lg:gap-5">
+          <div className="bg-white px-8 py-6 col-span-3 rounded-xl">
+            <h2 className="text-lg mb-2">Cash Flow</h2>
+            <hr className="border-[1px] my-5 opacity-50 border-dark-color bg-dark-color" />
+          </div>
+          <div className="bg-white p-8 rounded-xl">Two</div>
+        </div>
+      </div>
     </DashboardWrapper>
   );
 };
