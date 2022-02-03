f95e7404dcd29bf55f7646575cd05e42ea7f2dfe
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-03 12:34:39 +0300

    Create home card and updates

---
+++ src/components/HomeCard/HomeCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { HiDotsVertical, HiOutlineLocationMarker } from "react-icons/hi";
+import { FaBabyCarriage, FaCar } from "react-icons/fa";
+
+const HomeCard = () => {
+  return (
+    <div className="shadow-lg p-2 rounded-lg pb-5">
+      <img
+        className="w-full h-52 rounded-lg"
+        src="https://media.istockphoto.com/photos/home-with-blue-siding-and-stone-faade-on-base-of-home-picture-id1272128530?b=1&k=20&m=1272128530&s=170667a&w=0&h=k9lT5-DEmkmehDb-EKRHoP1-op2DTgz4ibiWGXmj7h8="
+        alt=""
+      />
+      <div className="mt-3 mb-2 flex justify-between">
+        <h2 className="text-dark-color font-bold">Real Estate</h2>
+        <HiDotsVertical className="text-2xl text-primary-blue cursor-pointer" />
+      </div>
+
+      <div className="flex items-center space-x-2 my-2">
+        <HiOutlineLocationMarker className="text-md text-primary-blue cursor-pointer" />
+        <p className="text-primary-blue text-sm font-light">Florida</p>
+      </div>
+
+      <div className="flex justify-between pr-5">
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2 my-2">
+            <FaCar className="text-md text-primary-blue cursor-pointer" />
+            <p className="text-dark-color text-sm font-light">5</p>
+          </div>
+          <div className="flex items-center space-x-2 my-2">
+            <FaBabyCarriage className="text-md text-primary-blue cursor-pointer" />
+            <p className="text-dark-color text-sm font-light">5</p>
+          </div>
+        </div>
+        <p className="text-dark-color font-medium font-sm">$1,700,000</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCard;
--- src/pages/Prospects/Prospects.jsx
+++ src/pages/Prospects/Prospects.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import DashboardWrapper from "../../components/DashboardWrapper/DashboardWrapper";
+import HomeCard from "../../components/HomeCard/HomeCard";
 
 const Prospects = () => {
   return (
     <DashboardWrapper>
-      <h4>Prospects</h4>
+      <div className="my-6 bg-white rounded-xl p-4 max-w-6xl">
+        <h2 className="text-md font-bold mb-2 ml-3 fo text-dark-color">
+          Prospects
+        </h2>
+        <hr className="border-0 h-[2px] my-2 opacity-50 border-dark-color bg-dark-color" />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+          <HomeCard />
+        </div>
+      </div>
     </DashboardWrapper>
   );
 };
