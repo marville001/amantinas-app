97082c788f7ca542270dac4daa453fd8bc64cd67
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-03 22:19:52 +0300

    Acrollable board

--- src/components/DashboardWrapper/DashboardWrapper.jsx
+++ src/components/DashboardWrapper/DashboardWrapper.jsx
@@ -6,7 +6,7 @@ const DashboardWrapper = ({ children, title }) => {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="pl-[320px] flex-1 px-6 py-8">
+      <div className="pl-[320px] flex-1 px-6 py-8 overflow-hidden">
         <Topbar title={title} />
         {children}
       </div>
--- src/pages/ProjectBoards/ProjectBoards.jsx
+++ src/pages/ProjectBoards/ProjectBoards.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { useState } from "react";
 import DashboardWrapper from "../../components/DashboardWrapper/DashboardWrapper";
 import HomeCard from "../../components/HomeCard/HomeCard";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 const ProjectBoards = () => {
-  const [selectedBoard, ] = useState("Board 1");
+  const [selectedBoard] = useState("Board 1");
   return (
     <DashboardWrapper
       title={`Project Boards - ${selectedBoard && selectedBoard}`}
     >
-      <div className="my-10 flex overflow-y-auto">
-        <div className="board bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
+      <ScrollContainer
+        className="
+      my-10 flex space-x-8 overflow-x-scroll board-container   
+      "
+      >
+        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
           <h2 className="text-md font-light ml-3 fo text-dark-color">
             Column 1
           </h2>
@@ -26,7 +31,39 @@ const ProjectBoards = () => {
             <HomeCard />
           </div>
         </div>
-      </div>
+
+        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
+          <h2 className="text-md font-light ml-3 fo text-dark-color">
+            Column 1
+          </h2>
+          <hr className="border-0 h-[2px] my-2 mb-16 opacity-50 border-dark-color bg-dark-color" />
+
+          <div className="w-72">
+            <HomeCard />
+          </div>
+        </div>
+
+        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
+          <h2 className="text-md font-light ml-3 fo text-dark-color">
+            Column 1
+          </h2>
+          <hr className="border-0 h-[2px] my-2 mb-16 opacity-50 border-dark-color bg-dark-color" />
+
+          <div className="w-72">
+            <HomeCard />
+          </div>
+          <div className="w-72">
+            <HomeCard />
+          </div>
+        </div>
+
+        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
+          <h2 className="text-md font-light ml-3 fo text-dark-color">
+            Column 1
+          </h2>
+          <hr className="border-0 h-[2px] my-2 mb-16 opacity-50 border-dark-color bg-dark-color" />
+        </div>
+      </ScrollContainer>
     </DashboardWrapper>
   );
 };
