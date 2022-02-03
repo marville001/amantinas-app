d7edcdded6d0c6cda623fae04b3b0431ca45e4fc
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-03 23:13:38 +0300

    Updates

--- src/pages/ProjectBoards/ProjectBoards.jsx
+++ src/pages/ProjectBoards/ProjectBoards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import DashboardWrapper from "../../components/DashboardWrapper/DashboardWrapper";
-import HomeCard from "../../components/HomeCard/HomeCard";
+import BoardCard from "../../components/BoardCard/BoardCard";
 import ScrollContainer from "react-indiana-drag-scroll";
 
 const ProjectBoards = () => {
@@ -19,16 +19,41 @@ const ProjectBoards = () => {
           <h2 className="text-md font-light ml-3 fo text-dark-color">
             Column 1
           </h2>
-          <hr className="border-0 h-[2px] my-2 mb-16 opacity-50 border-dark-color bg-dark-color" />
+          <hr className="border h-[2px] my-2 mb-8 opacity-50 border-dark-color bg-dark-color" />
 
-          <div className="w-72">
-            <HomeCard />
+          <div className="w-[90%] mx-auto">
+            <BoardCard />
           </div>
-          <div className="w-72">
-            <HomeCard />
+          <div className="w-[90%] mx-auto my-6">
+            <BoardCard />
           </div>
-          <div className="w-72">
-            <HomeCard />
+          <div className="w-[90%] mx-auto my-6">
+            <BoardCard />
+          </div>
+        </div>
+
+        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
+          <h2 className="text-md font-light ml-3 fo text-dark-color">
+            Column 2
+          </h2>
+          <hr className="border-0 h-[2px] my-2 mb-8 opacity-50 border-dark-color bg-dark-color" />
+
+          <div className="w-[90%] mx-auto">
+            <BoardCard />
+          </div>
+        </div>
+
+        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
+          <h2 className="text-md font-light ml-3 fo text-dark-color">
+            Column 3
+          </h2>
+          <hr className="border-0 h-[2px] my-2 mb-8 opacity-50 border-dark-color bg-dark-color" />
+
+          <div className="w-[90%] mx-auto">
+            <BoardCard />
+          </div>
+          <div className="w-[90%] mx-auto my-6">
+            <BoardCard />
           </div>
         </div>
 
@@ -36,32 +61,7 @@ const ProjectBoards = () => {
           <h2 className="text-md font-light ml-3 fo text-dark-color">
             Column 1
           </h2>
-          <hr className="border-0 h-[2px] my-2 mb-16 opacity-50 border-dark-color bg-dark-color" />
-
-          <div className="w-72">
-            <HomeCard />
-          </div>
-        </div>
-
-        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
-          <h2 className="text-md font-light ml-3 fo text-dark-color">
-            Column 1
-          </h2>
-          <hr className="border-0 h-[2px] my-2 mb-16 opacity-50 border-dark-color bg-dark-color" />
-
-          <div className="w-72">
-            <HomeCard />
-          </div>
-          <div className="w-72">
-            <HomeCard />
-          </div>
-        </div>
-
-        <div className="board min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl">
-          <h2 className="text-md font-light ml-3 fo text-dark-color">
-            Column 1
-          </h2>
-          <hr className="border-0 h-[2px] my-2 mb-16 opacity-50 border-dark-color bg-dark-color" />
+          <hr className="border-0 h-[2px] my-2 mb-8 opacity-50 border-dark-color bg-dark-color" />
         </div>
       </ScrollContainer>
     </DashboardWrapper>
