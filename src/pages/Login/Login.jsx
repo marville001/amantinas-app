cd4582bf7bc8d474165b8393d58ccdd0c1e585d6
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-05 22:58:34 +0300

    login and register responsive'

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,17 +7,17 @@ const Login = () => {
   const navigate = useNavigate();
   return (
     <div className="flex justify-center">
-      <div className="form w-[400px] py-12">
+      <div className="form px-6 sm:w-[400px] py-12">
         <h1 className="text-4xl text-center font-medium text-dark-color">
           Sign in to Amantinas.
         </h1>
         <div className="flex space-x-5 my-6 justify-center">
-          <div className="cursor-pointer p-2 px-12 rounded-lg bg-primary-blue text-white flex justify-center items-center">
-            <FaGoogle className="text-white font-bold text-3xl mr-2" />
+          <div className="cursor-pointer p-2 px-4 sm:px-12 rounded-lg bg-primary-blue text-white flex justify-center items-center">
+            <FaGoogle className="text-white font-bold text-xl sm:text-3xl mr-2" />
             <span className="text-sm">Sign in with Google</span>
           </div>
           <div className="cursor-pointer p-3 px-4 rounded-lg bg-white font-bold text-primary-blue">
-            <FaFacebookF className="text-primary-blue font-bold text-4xl mr-2" />
+            <FaFacebookF className="text-primary-blue font-bold text-xl sm:text-4xl mr-2" />
           </div>
         </div>
 
