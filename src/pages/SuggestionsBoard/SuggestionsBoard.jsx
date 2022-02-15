1b37b9d2bae988315b0bc1824d5287192a8ad186
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-15 14:11:38 +0300

    User cannot move suggestion item between boards

--- src/pages/SuggestionsBoard/SuggestionsBoard.jsx
+++ src/pages/SuggestionsBoard/SuggestionsBoard.jsx
@@ -1,15 +1,11 @@
 import { useEffect, useState } from "react";
-import { Droppable, Draggable, DragDropContext } from "react-beautiful-dnd";
-// import { FaSpinner } from "react-icons/fa";
+import { FaSpinner } from "react-icons/fa";
 import ScrollContainer from "react-indiana-drag-scroll";
 import { useDispatch, useSelector } from "react-redux";
 import DashboardWrapper from "../../components/DashboardWrapper/DashboardWrapper";
 import SuggestionModal from "../../components/Modals/SuggestionModal";
 import SuggestionBoardCard from "../../components/SuggestionBoardCard/SuggestionBoardCard";
-import {
-    loadSuggestionsAction,
-    suggestionDragAction,
-} from "../../redux/actions/suggestionsActions";
+import { loadSuggestionsAction } from "../../redux/actions/suggestionsActions";
 
 const initialData = [
     {
@@ -30,42 +26,14 @@ const initialData = [
 ];
 
 const SuggestionsBoard = () => {
-    const { suggestions } = useSelector((state) => state.suggestionsState);
+    const { suggestions, loading } = useSelector(
+        (state) => state.suggestionsState
+    );
 
     const [suggModalOpen, setSuggModalOpen] = useState(false);
     const [boardData, setBoardData] = useState(initialData);
 
     const dispatch = useDispatch();
-
-    const onDragEnd = async (re) => {
-        if (!re.destination) return;
-        let newBoardData = boardData;
-        var dragItem =
-            newBoardData[parseInt(re.source.droppableId)].items[
-                re.source.index
-            ];
-        newBoardData[parseInt(re.source.droppableId)].items.splice(
-            re.source.index,
-            1
-        );
-
-        newBoardData[parseInt(re.destination.droppableId)].items.splice(
-            re.destination.index,
-            0,
-            dragItem
-        );
-        setBoardData(newBoardData);
-
-        const columns = ["opinion", "planned", "progress"];
-        const destIndex = parseInt(re.destination.droppableId);
-        const column = columns[destIndex];
-
-        await dispatch(
-            suggestionDragAction({ column, suggestionId: dragItem._id })
-        );
-
-        console.log(column);
-    };
 
     useEffect(() => {
         dispatch(loadSuggestionsAction());
@@ -116,64 +84,38 @@ const SuggestionsBoard = () => {
                 </button>
             </div>
 
-            {/* {loading && (
+            {loading && (
                 <div className="flex justify-center my-4">
                     <FaSpinner className="animate-spin mr-4 text-2xl" />
                 </div>
-            )} */}
+            )}
 
             <ScrollContainer
-                ignoreElements=".board-item"
                 className="
       my-10 flex space-x-8 overflow-x-auto board-container select-none  
       "
             >
-                <DragDropContext onDragEnd={onDragEnd}>
-                    {[...boardData].map((board, idx) => (
-                        <div
-                            key={board.name}
-                            className="board flex-1 min-w-[200px] sm:min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl"
-                        >
-                            <Droppable droppableId={idx.toString()}>
-                                {(provided, snapshot) => (
-                                    <div
-                                        {...provided.droppableProps}
-                                        ref={provided.innerRef}
-                                        className="h-full pb-16"
-                                    >
-                                        <h2 className="text-md text-center py-3 font-light ml-3 fo text-dark-color">
-                                            {board.name}
-                                        </h2>
-                                        <hr className="border-0 h-[2px] my-2 mb-8 opacity-50 border-dark-color bg-dark-color" />
-                                        {board.items.length > 0 &&
-                                            board.items.map((item, idx) => (
-                                                <Draggable
-                                                    index={idx}
-                                                    draggableId={item._id.toString()}
-                                                    key={item._id}
-                                                >
-                                                    {(provided) => (
-                                                        <div
-                                                            className="board-item"
-                                                            ref={
-                                                                provided.innerRef
-                                                            }
-                                                            {...provided.draggableProps}
-                                                            {...provided.dragHandleProps}
-                                                        >
-                                                            <SuggestionBoardCard
-                                                                item={item}
-                                                            />
-                                                        </div>
-                                                    )}
-                                                </Draggable>
-                                            ))}
-                                    </div>
-                                )}
-                            </Droppable>
+                {[...boardData].map((board, idx) => (
+                    <div
+                        key={board.name}
+                        className="board flex-1 min-w-[200px] sm:min-w-[300px] bg-white p-2 flex flex-col max-h-[900px] overflow-y-auto rounded-3xl"
+                    >
+                        <div>
+                            <div className="h-full pb-16">
+                                <h2 className="text-md text-center py-3 font-light ml-3 fo text-dark-color">
+                                    {board.name}
+                                </h2>
+                                <hr className="border-0 h-[2px] my-2 mb-8 opacity-50 border-dark-color bg-dark-color" />
+                                {board.items.length > 0 &&
+                                    board.items.map((item, idx) => (
+                                        <div key={idx} className="board-item">
+                                            <SuggestionBoardCard item={item} />
+                                        </div>
+                                    ))}
+                            </div>
                         </div>
-                    ))}
-                </DragDropContext>
+                    </div>
+                ))}
             </ScrollContainer>
 
             <SuggestionModal
