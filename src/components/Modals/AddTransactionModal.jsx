3b49f96d95c0b1243a4ebb5ecd598aa4e423360b
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-17 11:31:05 +0300

    Add transaction design done

--- src/components/Modals/AddTransactionModal.jsx
+++ src/components/Modals/AddTransactionModal.jsx
@@ -50,7 +50,7 @@ const AddTransactionModal = ({ isOpen, size, closeModal = () => {} }) => {
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
                     className="
-        outline-none p-1 px-2 text-sm rounded bg-light-blue ring-1 
+        outline-none p-1 px-2 text-sm !rounded bg-light-blue ring-1 
         ring-dark-color text-dark-color
         "
                 />
@@ -62,7 +62,7 @@ const AddTransactionModal = ({ isOpen, size, closeModal = () => {} }) => {
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
                     className="
-        outline-none p-1 text-sm rounded px-2 bg-light-blue ring-1 
+        outline-none p-1 text-sm !rounded px-2 bg-light-blue ring-1 
         ring-dark-color text-dark-color
         "
                 />
@@ -71,29 +71,109 @@ const AddTransactionModal = ({ isOpen, size, closeModal = () => {} }) => {
             <div className="flex flex-1 flex-col space-y-2 my-2">
                 <label className="text-white text-md">Amount</label>
                 <input
-                    type="text"
+                    type="number"
                     value={amount}
                     onChange={(e) => setAmount(e.target.value)}
                     className="
-        outline-none p-1 text-sm rounded px-2 bg-light-blue ring-1 
+        outline-none p-1 text-sm !rounded px-2 bg-light-blue ring-1 
         ring-dark-color text-dark-color
         "
                 />
             </div>
 
+            <div className="flex flex-1 flex-col space-y-2 my-2">
+                <label className="text-white text-md">
+                    Date of transaction
+                </label>
+                <input
+                    type="date"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    className="
+        outline-none p-1 text-sm !rounded px-2 bg-light-blue ring-1 
+        ring-dark-color text-dark-color
+        "
+                />
+            </div>
+
+            <div className="flex flex-1 flex-col space-y-2 my-2">
+                <label className="text-white text-md">
+                    Type of transaction
+                </label>
+                <select
+                    value={type}
+                    onChange={(e) => setType(e.target.value)}
+                    className="
+        outline-none p-1 text-sm rounded px-2 bg-light-blue ring-1 
+        ring-dark-color text-dark-color
+        "
+                >
+                    <option value="">Select Type</option>
+                    <option value="income">Income</option>
+                    <option value="expense">Expense</option>
+                </select>
+            </div>
+
+            <div className="flex flex-1 flex-col space-y-2 my-2 py-2">
+                <label className="text-white text-md">
+                    <input
+                        type="checkbox"
+                        value={recurring}
+                        onChange={(e) => setRecurring(e.target.checked)}
+                        className="
+        outline-none p-1 text-sm !rounded px-2 bg-light-blue ring-1 
+        ring-dark-color mr-2
+        "
+                    />
+                    Recurring Transaction?
+                </label>
+            </div>
+            {recurring && (
+                <div className="flex space-x-2">
+                    <div className="flex flex-1 flex-col space-y-2 my-2">
+                        <label className="text-white text-md">Start Date</label>
+                        <input
+                            type="date"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            className="
+        outline-none p-1 text-sm !rounded px-2 bg-light-blue ring-1 
+        ring-dark-color text-dark-color
+        "
+                        />
+                    </div>
+
+                    <div className="flex flex-1 flex-col space-y-2 my-2">
+                        <label className="text-white text-md">
+                            End Date{" "}
+                            <span className="text-xs"> (optional)</span>
+                        </label>
+                        <input
+                            type="date"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                            className="
+        outline-none p-1 text-sm !rounded px-2 bg-light-blue ring-1 
+        ring-dark-color text-dark-color
+        "
+                        />
+                    </div>
+                </div>
+            )}
+
             <div className="flex  space-y-2 my-4 justify-center">
                 <button
-                    disabled={true}
+                    disabled={false}
                     onClick={handleSubmit}
                     className="disabled:opacity-50 disabled:cursor-not-allowed uppercase px-16 tracking-wider py-2 bg-dark-color text-white text-lg rounded-md mt-8 flex items-center"
                 >
-                    {true ? (
+                    {false ? (
                         <>
                             <FaSpinner className="animate-spin mr-4" />{" "}
                             <span className="capitalize">Loading...</span>
                         </>
                     ) : (
-                        <span>Create</span>
+                        <span>Submit</span>
                     )}
                 </button>
             </div>
