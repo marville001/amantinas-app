4c4e3057e5c831160a62c886a9f21caadfdcd039
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-17 13:32:12 +0300

    load homes

--- src/pages/Ledger/Ledger.jsx
+++ src/pages/Ledger/Ledger.jsx
@@ -1,51 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import DashboardWrapper from "../../components/DashboardWrapper/DashboardWrapper";
 
 import DataTable from "../../components/DataTable/DataTable";
 import AddTransactionModal from "../../components/Modals/AddTransactionModal";
+import { getHomesAction } from "../../redux/actions/homesActions";
 
-const Ledger = () => {    
-  const [addTransModalOpen, setAddTransModalOpen] = useState(false);
+const Ledger = () => {
+    const { user } = useSelector((state) => state.userAuthState);
 
-  return (
-    <DashboardWrapper title={"Ledger"}>
-      <div className="my-6">
-        <DataTable
-          title="Activities"
-          btnText="Add Transaction"
-          columnTitles={[
-            "Account",
-            "Date",
-            "Merchant",
-            "Amount",
-            "Category",
-            "Property",
-            "Notes",
-          ]}
-          btnAction={() => setAddTransModalOpen(true)}
-        />
-      </div>
-      <div className="my-6">
-        <DataTable
-          title="Invoices"
-          btnText="Add Invoice"
-          columnTitles={[
-            "Account",
-            "Date",
-            "Type",
-            "Amount",
-            "Category",
-            "Property",
-            "Notes",
-          ]}
-        />
-      </div>
-      <AddTransactionModal
+    const [addTransModalOpen, setAddTransModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getHomesAction({ investorId: user?._id }));
+    }, [dispatch, user?._id]);
+
+    return (
+        <DashboardWrapper title={"Ledger"}>
+            <div className="my-6">
+                <DataTable
+                    title="Activities"
+                    btnText="Add Transaction"
+                    columnTitles={[
+                        "Account",
+                        "Date",
+                        "Merchant",
+                        "Amount",
+                        "Category",
+                        "Property",
+                        "Notes",
+                    ]}
+                    btnAction={() => setAddTransModalOpen(true)}
+                />
+            </div>
+            <div className="my-6">
+                <DataTable
+                    title="Invoices"
+                    btnText="Add Invoice"
+                    columnTitles={[
+                        "Account",
+                        "Date",
+                        "Type",
+                        "Amount",
+                        "Category",
+                        "Property",
+                        "Notes",
+                    ]}
+                />
+            </div>
+            <AddTransactionModal
                 isOpen={addTransModalOpen}
                 closeModal={() => setAddTransModalOpen(false)}
             />
-    </DashboardWrapper>
-  );
+        </DashboardWrapper>
+    );
 };
 
 export default Ledger;
