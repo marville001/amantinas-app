8afab92b9192ffafcc70e8831353c46aba0b7cdf
Martin Mwangi <mwangimartin1904@gmail.com>
2022-02-18 13:20:09 +0300

    Add admin done

---
+++ src/admin/components/AddAdminModal.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { FaSpinner } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import Modal from "../../components/Modals/Modal";
+import { createAdminAction } from "../../redux/actions/adminActions";
+
+const AddAdminModal = ({ isOpen, closeModal = () => {} }) => {
+    const { isCreatingAdmin } = useSelector(
+        (state) => state.adminState
+    );
+    const { admin } = useSelector((state) => state.adminState);
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const dispatch = useDispatch();
+
+    const handleCloseModal = () => {
+        setEmail("");
+        setName("");
+        setPassword("");
+        closeModal();
+    };
+
+    const handleSubmit = async () => {
+        setError("");
+        const obj = {
+            name,
+            email,
+            password,
+        };
+
+        const res = await dispatch(createAdminAction(obj, "admin"));
+        if (!res.success) {
+            setError(res.message);
+        } else {
+            handleCloseModal();
+        }
+    };
+
+    return (
+        <Modal size="sm" isOpen={isOpen} closeModal={handleCloseModal}>
+            <div className="text-white text-2xl text-center mb-6">
+                Add Admin
+            </div>
+            {error && (
+                <div className="text-center bg-red-200 rounded-lg text-red-500 my-4 text-sm p-1">
+                    {error}
+                </div>
+            )}
+            <div className="flex flex-col space-y-2">
+                <label className="text-white">Name</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="
+        outline-none p-2 py-1 text-sm !rounded bg-light-blue ring-1 
+        ring-dark-color text-dark-color
+        "
+                />
+            </div>
+            <div className="flex flex-col space-y-2 my-4">
+                <label className="text-white">Email</label>
+                <input
+                    type="text"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="
+        outline-none p-2 py-1 text-sm !rounded bg-light-blue ring-1 
+        ring-dark-color text-dark-color
+        "
+                />
+            </div>
+            <div className="flex flex-col space-y-2">
+                <label className="text-white">Password</label>
+                <input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="
+        outline-none p-2 py-1 text-sm !rounded bg-light-blue ring-1 
+        ring-dark-color text-dark-color
+        "
+                />
+            </div>
+            <div className="flex  space-y-2 justify-center">
+                <button
+                    disabled={isCreatingAdmin}
+                    onClick={handleSubmit}
+                    className="disabled:opacity-50 disabled:cursor-not-allowed uppercase px-16 tracking-wider py-2 bg-dark-color text-white text-lg rounded-md mt-8 flex items-center"
+                >
+                    {isCreatingAdmin ? (
+                        <>
+                            <FaSpinner className="animate-spin mr-4" />{" "}
+                            <span className="capitalize">Loading...</span>
+                        </>
+                    ) : (
+                        <span>Submit</span>
+                    )}
+                </button>
+            </div>
+        </Modal>
+    );
+};
+
+export default AddAdminModal;
--- src/admin/pages/AdminHome.jsx
+++ src/admin/pages/AdminHome.jsx
@@ -1,12 +1,111 @@
-import React from 'react'
-import DashboardWrapper from '../Wrapper'
+import React, { useState } from "react";
+import { FaAirbnb } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import AddAdminModal from "../components/AddAdminModal";
+import DashboardWrapper from "../Wrapper";
 
 const AdminHome = () => {
-  return (
-    <DashboardWrapper title="Archived Data">
-<h2>Admin Home</h2>
-    </DashboardWrapper>
-  )
-}
+    const [addAdminModalOpen, setAddAdminModalOpen] = useState(false);
+    return (
+        <DashboardWrapper title="Archived Data">
+            <div className="mb-10 mt-4 flex md:space-x-2 space-y-2 sm:space-x-2 sm:space-y-0 flex-col sm:flex-row">
+                <div className="flex-[1] bg-white p-4">
+                    <h4>Welcome "{"Admin Name"}"</h4>
+                    <div className="flex items-center mt-6 justify-between">
+                        <p className="text-dark-color">Total no of investors</p>
+                        <div className="text-2xl text-dark-color font-bold">
+                            450
+                        </div>
+                    </div>
+                    <Link
+                        to="/admin"
+                        className="mt-3 rounded uppercase tracking-wider text-xs inline-block 
+                        bg-primary-blue text-white py-2 px-6"
+                    >
+                        Manage Investors
+                    </Link>
+                </div>
+                <div className="flex-[2] bg-white p-4">
+                    <h4 className="text-dark-color font-bold">Analytics</h4>
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4 mt-8">
+                        <div className="flex items-center">
+                            <div className="rounded-full bg-light-blue p-2">
+                                <FaAirbnb className="text-2xl font-bold" />
+                            </div>
+                            <div className="ml-2 flex flex-col space-y-0">
+                                <h3 className="font-bold text-2xl text-primary-blue">
+                                    24
+                                </h3>
+                                <p className="text-sm">Fee Plan Users</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="rounded-full bg-light-blue p-2">
+                                <FaAirbnb className="text-2xl font-bold" />
+                            </div>
+                            <div className="ml-2 flex flex-col space-y-0">
+                                <h3 className="font-bold text-2xl text-primary-blue">
+                                    24
+                                </h3>
+                                <p className="text-sm">Elite Plan Users</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="rounded-full bg-light-blue p-2">
+                                <FaAirbnb className="text-2xl font-bold" />
+                            </div>
+                            <div className="ml-2 flex flex-col space-y-0">
+                                <h3 className="font-bold text-2xl text-primary-blue">
+                                    24
+                                </h3>
+                                <p className="text-sm">Pro Plan Users</p>
+                            </div>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="rounded-full bg-light-blue p-2">
+                                <FaAirbnb className="text-2xl font-bold" />
+                            </div>
+                            <div className="ml-2 flex flex-col space-y-0">
+                                <h3 className="font-bold text-2xl text-primary-blue">
+                                    5
+                                </h3>
+                                <p className="text-sm">Admin Users</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white p-4">
+                <div className="flex items-center justify-between w-full sm:w-1/2 mt-4">
+                    <h2>Admins</h2>
+                    <button
+                        onClick={() => setAddAdminModalOpen(true)}
+                        className="bg-dark-color py-1 px-6 text-sm text-white rounded-md"
+                    >
+                        Add Admin
+                    </button>
+                </div>
+                <table className="w-full sm:w-1/2 mt-4">
+                    <thead className="mb-2">
+                        <tr>
+                            <th className="text-left">Email</th>
+                            <th className="text-left">Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>malcolmockyer@gmail.com</td>
+                            <td>Malcool Mockyer</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <AddAdminModal
+                isOpen={addAdminModalOpen}
+                closeModal={() => setAddAdminModalOpen(false)}
+            />
+        </DashboardWrapper>
+    );
+};
 
-export default AdminHome
+export default AdminHome;
