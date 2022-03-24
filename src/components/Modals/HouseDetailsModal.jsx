1b0300fb522fef7cbf2095fdba9a7f6de102099a
Martin Mwangi <mwangimartin1904@gmail.com>
2022-03-24 11:53:16 +0300

    Mortgage calculator done

--- src/components/Modals/HouseDetailsModal.jsx
+++ src/components/Modals/HouseDetailsModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import priceFormatter from "../../utils/priceFormatter";
@@ -12,15 +12,26 @@ import "swiper/css/navigation";
 import { Navigation } from "swiper";
 
 const HouseDetailsModal = ({ isOpen, closeModal = () => {}, home }) => {
-    const [interestRate, setInterestRate] = useState(4.001);
+    const [interestRate, setInterestRate] = useState(4);
     const [termInYears, setTermInYears] = useState(30);
+    const [downPaymentAmount, setDownPaymentAmount] = useState(30);
     const downPaymentPercent = 0.2;
-    const downPaymentAmount = 60000;
     const loanAmount = 240000;
 
     const calculateClosingCost = () => {
         return 400;
     };
+
+    const claculateMorgage = () => {
+        const mortPrice =
+            ((home.price - downPaymentAmount) * interestRate) / 360;
+        return mortPrice;
+    };
+
+    useEffect(() => {
+        setDownPaymentAmount(home.price * downPaymentPercent);
+    }, [home.price, downPaymentPercent]);
+
     return (
         <Modal
             center={false}
@@ -72,12 +83,47 @@ const HouseDetailsModal = ({ isOpen, closeModal = () => {}, home }) => {
             <div className="flex gap-4">
                 <div className="flex-[1]">
                     <h4 className="text-white my-1">Home Price ($)</h4>
-                    <input type="number" value={home.price} />
+                    <input type="number" className="disabled:bg-brown-color disabled:text-white outline-none ring-0 border-0 py-1" disabled value={home.price} />
 
-                    <label htmlFor="interestRate" className="text-white mt-4 block my-1">Interest Rate (%)</label>
-                    <input type="number" className="!w-auto inline" step={0.001} onChange={e=>setInterestRate(e.target.value)} value={interestRate} />
+                    <h4 className="text-white my-1">Down Payment ($)</h4>
+                    <input
+                        type="number"
+                        className="py-1"
+                        value={downPaymentAmount}
+                    />
+
+                    <label className="text-white mt-4 block my-1">
+                        Interest Rate (%)
+                    </label>
+                    <input
+                        type="number"
+                        className="!w-auto inline py-1"
+                        step={0.001}
+                        onChange={(e) => setInterestRate(e.target.value)}
+                        value={interestRate}
+                    />
                 </div>
-                <div className="flex-[1]">hhfhf</div>
+                <div className="flex-[1]">
+                    <div className="ring-2 ring-[#eee] rounded p-2">
+                        <h2 className="text-brown-color">
+                            Mortgage Calculator{" "}
+                        </h2>
+
+                        <div className="mt-2 text-2xl text-white">
+                            {priceFormatter(claculateMorgage())}
+                        </div>
+                    </div>
+
+                    <div className="ring-2 ring-[#eee] rounded p-2 mt-2">
+                        <h2 className="text-brown-color">
+                            Projected Rent Calculator{" "}
+                        </h2>
+
+                        <div className="mt-2 text-2xl text-white">
+                            {priceFormatter(home.price * 0.014)}
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className=" mt-6 flex justify-between">
